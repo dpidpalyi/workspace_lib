aa270c4a2967683cb50baec581f36430d916d6c6
dpidpalyi <dpidpalyi@mail.ru>
2024-10-05 15:14:51 +0500

    add comments for godoc

--- lib.go
+++ lib.go
@@ -1,9 +1,14 @@
+// Package workspace_lib represents functions to work with numbers
+//
+//[MathIsFun]: https://www.mathisfun.com/numbers/
 package workspace_lib
 
+// AddNums sumarize two numbers
 func AddNums(a, b int) int {
 	return a + b
 }
 
+// SubNums return difference from two numbers
 func SubNums(a, b int) int {
 	return a - b
 }
