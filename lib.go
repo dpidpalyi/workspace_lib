b706d8a26d3a1ef00e799377be4661eadb7db9d0
dpidpalyi <dpidpalyi@mail.ru>
2024-10-05 15:22:16 +0500

    v1

--- lib.go
+++ lib.go
@@ -1,14 +1,22 @@
 // Package workspace_lib represents functions to work with numbers
+// v1 use generic
 //
 //[MathIsFun]: https://www.mathisfun.com/numbers/
 package workspace_lib
 
+import "golang.org/x/exp/constraints"
+
+// Number represents constraints for numbers
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // AddNums sumarize two numbers
-func AddNums(a, b int) int {
+func AddNums[T Number](a, b T) T {
 	return a + b
 }
 
 // SubNums return difference from two numbers
-func SubNums(a, b int) int {
+func SubNums[T Number](a, b T) T {
 	return a - b
 }
